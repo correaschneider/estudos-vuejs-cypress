ca735869a85315f6e49de15797389d5562b1c6e1
Pedro Schneider <pedro.schneider@cwi.com.br>
2020-01-14 19:37:51 -0300

    Adicionando teste da Aula 4

---
+++ cypress/integration/aula4_spec.js
@@ -0,0 +1,30 @@
+context('Aula 4', () => {
+  beforeEach(() => {
+    cy.visit('http://httpd/aula4')
+  })
+
+  it('Interagindo com Aula 4', function () {
+    let ul = cy
+        .get('ul')
+
+    ul
+        .get('li:eq(0)')
+        .contains('JavaScript')
+
+    ul
+        .get('li:eq(1)')
+        .contains('PHP')
+
+    ul
+        .get('li:eq(2)')
+        .contains('C#')
+
+    ul
+        .get('li:eq(3)')
+        .contains('Java')
+
+    ul
+        .get('li:eq(4)')
+        .contains('Python')
+  })
+})
