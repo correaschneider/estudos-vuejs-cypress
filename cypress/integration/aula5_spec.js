6152bedd8b9199c3f45410d99bc47f7282951e1d
Pedro Schneider <correaschneider@gmail.com>
2020-01-15 00:18:57 -0300

    Finalizando teste da Aula 5

--- cypress/integration/aula5_spec.js
+++ cypress/integration/aula5_spec.js
@@ -1,12 +1,47 @@
-context('Aula 5', () => {
-  beforeEach(() => {
-    cy.visit('http://httpd/aula5')
-  })
+describe('Aula 5', () => {
+	it('Interagindo com Aula 5', function () {
+		cy.visit('http://httpd/aula5/')
 
-  it('Interagindo com Aula 5', function () {
-    let ul = cy
-        .get('#name')
-        .type('Teste')
+		cy
+			.get('#name')
+			.type('Carolina Fernandes Oliveira')
 
-  })
+		cy
+			.get('#phone')
+			.type('091977950117')
+
+		cy
+			.get('[name=news]')
+			.check(['Sim'])
+
+		cy
+			.get('[name=interest]')
+			.check(['Futebol', 'Basquetebol'])
+
+		cy
+			.get('#know')
+			.select('Google')
+
+		cy.screenshot()
+
+		cy
+			.get('#name')
+			.should('have.value', 'Carolina Fernandes Oliveira')
+
+		cy
+			.get('#phone')
+			.should('have.value', '091977950117')
+
+		cy
+			.get('#news-sim')
+			.should('be.checked')
+
+		cy
+			.get('[name=interest][value=Futebol],[name=interest][value=Basquetebol]')
+			.should('be.checked')
+
+		cy
+			.get('#know')
+			.should('have.value', 'Google')
+	})
 })
